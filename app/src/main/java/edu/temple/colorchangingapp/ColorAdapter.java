a8a9615b9e2a6d3be1e8cc65627ece8b14472b7d
jdw <jdw@notarealaddress.com>
2020-10-07 17:23:44 -0400

    Created a custom adapter, ColorAdapter, similar to the one made for our previous assignment.

---
+++ app/src/main/java/edu/temple/colorchangingapp/ColorAdapter.java
@@ -0,0 +1,44 @@
+package edu.temple.colorchangingapp;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class ColorAdapter extends BaseAdapter {
+
+    Context context;
+    ArrayList<String> items;
+
+    public ColorAdapter(Context context, ArrayList<String> items){
+        this.context = context;
+        this.items = items;
+    }
+
+    @Override
+    public int getCount() {
+        return items.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return items.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        TextView textView = new TextView(context);
+        textView.setText(getItem(position).toString());
+        textView.setBackgroundColor(Color.parseColor(getItem(position).toString().toLowerCase()));
+        return textView;
+    }
+}
