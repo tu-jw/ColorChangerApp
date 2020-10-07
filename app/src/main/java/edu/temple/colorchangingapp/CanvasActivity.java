2db290a863c5dbc2c235794f1bdf5f380c45a736
jdw <jdw@notarealaddress.com>
2020-10-07 19:42:02 -0400

    Added the funcitonality of the custom adapter to the grid view created. Basic functionality success checker in place in the form of text confirmation.

--- app/src/main/java/edu/temple/colorchangingapp/CanvasActivity.java
+++ app/src/main/java/edu/temple/colorchangingapp/CanvasActivity.java
@@ -1,14 +1,29 @@
 package edu.temple.colorchangingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class CanvasActivity extends AppCompatActivity {
+
+    ConstraintLayout cLayout;
+    TextView colorText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_canvas);
+
+        Intent startIntent = getIntent();
+        String colorRecieved = startIntent.getStringExtra("color_selected");
+
+        cLayout = findViewById(R.id.constraintLayout);
+        colorText = findViewById(R.id.colorTextView);
+        colorText.setText(colorRecieved.toUpperCase());
+        cLayout.setBackgroundColor(Color.parseColor(colorRecieved));
     }
 }
--- app/src/main/java/edu/temple/colorchangingapp/PaletteActivity.java
+++ app/src/main/java/edu/temple/colorchangingapp/PaletteActivity.java
@@ -2,13 +2,61 @@ package edu.temple.colorchangingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class PaletteActivity extends AppCompatActivity {
+
+    private int CODE = 11;
+
+    // Our structures to be set in onCreate() method
+    ArrayList<String> colors;
+    GridView gridView;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        colors = new ArrayList();
+        gridView = findViewById(R.id.colorGrid);
+        textView = findViewById(R.id.greeting);
+
+        colors.add("WHITE");
+        colors.add("GREY");
+        colors.add("FUCHSIA");
+        colors.add("RED");
+        colors.add("YELLOW");
+        colors.add("LIME");
+        colors.add("GREEN");
+        colors.add("CYAN");
+        colors.add("BLUE");
+
+        final BaseAdapter adapter = new ColorAdapter(this, colors);
+
+        gridView.setAdapter(adapter);
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Object selectedThing = adapterView.getItemAtPosition(i);
+                // textView.setText(selectedThing.toString().toLowerCase());
+
+                Intent canvasActivityIntent = new Intent(PaletteActivity.this, CanvasActivity.class);
+                canvasActivityIntent.putExtra("color_selected", (selectedThing.toString().toLowerCase()));
+                startActivity(canvasActivityIntent); // this will switch to the target specified here ^^^ in our Intent creation and switch to it.
+            }
+        });
+
     }
 }
