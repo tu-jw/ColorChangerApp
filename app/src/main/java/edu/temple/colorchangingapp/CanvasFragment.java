5741a4f0d941f9968323263ce59bddf32838e836
jdw <jdw@notarealaddress.com>
2020-10-21 20:21:40 -0400

    Began outlining the Canvas Fragment's functionality.

--- app/src/main/java/edu/temple/colorchangingapp/CanvasFragment.java
+++ app/src/main/java/edu/temple/colorchangingapp/CanvasFragment.java
@@ -1,5 +1,6 @@
 package edu.temple.colorchangingapp;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,10 +8,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 // This is the "details" fragment
 public class CanvasFragment extends Fragment {
 
+    Context thisContext;
+    TextView textView;
+    View layout;
 
     public CanvasFragment() {
         // Required empty public constructor
@@ -20,7 +25,14 @@ public class CanvasFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        thisContext = getContext();
+        layout = inflater.inflate(R.layout.fragment_canvas, container, false);
+        textView = layout.findViewById(R.id.greeting); // sets the textView equal to our greeting text
+        textView.setText("Choose a color!");
+
+        // Receive the color selected from the adapter in the other fragment (palette fragment) and set the text = that... Also set the background color = that. Handle French
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_canvas, container, false);
+        return layout;
     }
 }
